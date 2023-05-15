79ccf1802bdfaf37a04fe464d3225c8271a4d079
mahesh-k-simformsolutions <mahesh.k@simformsolutions.com>
2023-05-15 09:40:02 +0530

    changed dbcontext scope lifetime logging level set

--- ExamManagementSystem/Program.cs
+++ ExamManagementSystem/Program.cs
@@ -18,7 +18,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString, opt => { opt.EnableRetryOnFailure(); }), ServiceLifetime.Singleton);
+    options.UseSqlServer(connectionString));
 
 builder.Logging.AddFile($"Logs/{Assembly.GetExecutingAssembly().GetName().Name}.log");
 
